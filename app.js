150d14609d5cfa244a3b91b26b0f69519f6da0db
blvckcoffee <joe.johnson.dev@gmail.com>
2021-01-25 16:31:47 -0600

    Finish javascript variables exercises

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n')
 console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+  'Follow the steps in the README.md file to complete the exercises:\n==========\n'
+)
 
 // Exercise 1
 
+const FIRST_NAME = 'Joe'
+let lastName = 'Johnson'
+var age = 31
+
 // Exercise 2
 
+//let fullName = FIRST_NAME + ' ' + lastName
+//console.log(fullName)
+let fullName = `${FIRST_NAME} ${lastName}`
+console.log(fullName)
+
 // Exercise 3
+
+let city = 'Birmingham, AL'
+let pastime = 'long distance running'
+let myStory = `Hello, everyone. My name is ${fullName}. I live in ${city}. I enjoy ${pastime}, reading, and spending time with my wife.`
+console.log(myStory)
